e43ab760f26954eef9dcb4572e4e084747255f34
Rooyalotfi <roya.lotfi93@gmail.com>
2019-12-13 22:22:27 -0600

    cleared code and added comment and names were corrected

--- test.py
+++ test.py
@@ -1,25 +1,24 @@
 from typing import Tuple
-import numpy as np
 import string
 
 
 class Node:
 
-    def __init__(self, data, nextNode=None):
+    def __init__(self, data, next_node=None):
         self.data = data
-        self.nextNode = nextNode
+        self.next_node = next_node
 
-    def getData(self):
+    def get_data(self):
         return self.data
 
-    def setData(self, val):
+    def set_data(self, val):
         self.data = val
 
-    def getNextNode(self):
-        return self.nextNode
+    def get_next_node(self):
+        return self.next_node
 
-    def setNextNode(self, val):
-        self.nextNode = val
+    def set_next_node(self, val):
+        self.next_node = val
 
 
 class LinkedList:
@@ -30,61 +29,60 @@ class LinkedList:
 
     def make_alphabet_list(self):
         for l in list(string.ascii_lowercase):
-            newNode = Node(l, self.head)
-            self.head = newNode
+            new_node = Node(l, self.head)
+            self.head = new_node
             self.size += 1
 
     def get_size(self):
         return self.size
 
-    def addNode(self, data):
+    def add_node(self, data):
         curr = self.head
         while curr:
-            if curr.getData() == data:
+            if curr.get_data() == data:
                 return False, "already in the list"
-            curr = curr.getNextNode()
+            curr = curr.get_next_node()
 
-        newNode = Node(data, self.head)
-        self.head = newNode
+        new_node = Node(data, self.head)
+        self.head = new_node
         self.size += 1
         return True, "Added to the list"
 
-    def printNode(self):
+    def print_node(self):
         curr = self.head
         while curr:
-            print(curr.getData())
-            curr = curr.getNextNode()
+            print(curr.get_data())
+            curr = curr.get_next_node()
 
-    def removeNode(self, value):
-
+    def remove_node(self, value):
         prev = None
         curr = self.head
         while curr:
-            if curr.getData() == value:
+            if curr.get_data() == value:
                 if prev:
-                    prev.setNextNode(curr.getNextNode())
+                    prev.set_next_node(curr.get_next_node())
                 else:
-                    self.head = curr.getNextNode()
+                    self.head = curr.get_next_node()
                 return True
 
             prev = curr
-            curr = curr.getNextNode()
+            curr = curr.get_next_node()
         return False
 
-    def findNode(self, value):
+    def find_node(self, value):
         curr = self.head
         while curr:
-            if curr.getData() == value:
+            if curr.get_data() == value:
                 return True
-            curr = curr.getNextNode()
+            curr = curr.get_next_node()
         return False
 
     def get_list(self):
         curr = self.head
         all_list = []
         while curr:
-            all_list.append(curr.getData())
-            curr = curr.getNextNode()
+            all_list.append(curr.get_data())
+            curr = curr.get_next_node()
         return all_list
 
 
@@ -94,7 +92,7 @@ class TrieNode(object):
     """
     counter = 0
 
-    def __init__(self, char: str):
+    def __init__(self, char):
         self.char = char
         self.children = []
         # Is it the last character of the word.`
@@ -103,28 +101,21 @@ class TrieNode(object):
         self.counter = 1
 
 
-def add(root, word: str):
+def add(root_node, word: str):
     """
-    Adding a word in the trie structure
+    Adding a word in the trie data structure
     """
-    node = root
+    node = root_node
+    found_in_child = False
     for char in word:
         found_in_child = False
         # Search for the character in the children of the present `node`
         for child in node.children:
             if child.char == char:
-                #print("character is : ", char)
-                #print("node.child is :", child.char)
-                #print(child.char == char)
-                #print("\n")
-                # We found it, increase the counter by 1 to keep track that another
-                # word has it as well
-                # And point the node to the child that contains this char
-
                 node = child
                 found_in_child = True
                 break
-        # We did not find it so add a new chlid
+        # We did not find it so add a new child
         if not found_in_child:
             new_node = TrieNode(char)
             node.children.append(new_node)
@@ -139,11 +130,11 @@ def add(root, word: str):
     node.word_finished = True
 
 
-def _add(root, word: str):
+def _add(root_node, word: str):
     """
     Adding a word in the trie structure
     """
-    node = root
+    node = root_node
     for char in word:
         found_in_child = False
         # Search for the character in the children of the present `node`
@@ -156,7 +147,7 @@ def _add(root, word: str):
                 node = child
                 found_in_child = True
                 break
-        # We did not find it so add a new chlid
+        # We did not find it so add a new child
         if not found_in_child:
             new_node = TrieNode(char)
             node.children.append(new_node)
@@ -166,16 +157,16 @@ def _add(root, word: str):
     node.word_finished = True
 
 
-def find_prefix(root, prefix: str) -> Tuple[bool, int]:
+def find_prefix(root_node, prefix: str) -> Tuple[bool, int]:
     """
     Check and return
       1. If the prefix exsists in any of the words we added so far
       2. If yes then how may words actually have the prefix
     """
-    node = root
+    node = root_node
     # If the root node has no children, then return False.
     # Because it means we are trying to search in an empty trie
-    if not root.children:
+    if not root_node.children:
         return False, 0
     for char in prefix:
         char_not_found = True
@@ -195,16 +186,16 @@ def find_prefix(root, prefix: str) -> Tuple[bool, int]:
     # prefix
     return True, node.counter
 
-def is_in_list(root, prefix: str):
+
+def is_in_list(root_node, prefix: str):
     """
     Check and return
       1. If the prefix exsists in any of the words we added so far
-      2. If yes then how may words actually have the prefix
     """
-    node = root
+    node = root_node
     # If the root node has no children, then return False.
     # Because it means we are trying to search in an empty trie
-    if not root.children:
+    if not root_node.children:
         return False, 0
     for char in prefix:
         char_not_found = True
@@ -224,15 +215,16 @@ def is_in_list(root, prefix: str):
     # prefix
     return True
 
-def find_weight(root, prefix: str):
+
+def find_weight(root_node, prefix: str):
     """
     Check and return
       2. If yes then how may words actually have the prefix
     """
-    node = root
+    node = root_node
     # If the root node has no children, then return False.
     # Because it means we are trying to search in an empty trie
-    if not root.children:
+    if not root_node.children:
         return 0
     for char in prefix:
         char_not_found = True
@@ -252,14 +244,14 @@ def find_weight(root, prefix: str):
     # prefix
     return node.counter
 
+
 def bubble_sort(nums):
     # We set swapped to True so the loop looks runs at least once
     swapped = True
     while swapped:
         swapped = False
         for i in range(len(nums) - 1):
-            #print(find_weight(root, nums[i]) < find_weight(root,nums[i + 1]))
-            if (find_weight(root, nums[i]) > find_weight(root,nums[i + 1])):
+            if find_weight(root, nums[i]) > find_weight(root,nums[i + 1]):
                 # Swap the elements
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
                 # Set the flag to True so we'll loop again
@@ -267,113 +259,96 @@ def bubble_sort(nums):
     return nums
 
 
-def update_list(myList,str, index):
-    j = len(str) - index + 1
-    max = -1
-    str_max = ""
-    candid = []
+def update_list(linked_list, str, context_len):
+    j = len(str) - context_len + 1
+    max_weight = -1
+    candid_items = []
+    list_iter = linked_list.get_list()
 
-    list_iter = myList.get_list();
-    while j < len(str)+1:
-        # if candid is only one item then we need to sort them based on their number
-        # of occurance in the input that is why we separate them
-        if (j <len(str)):
-            candid = []
+    while j <= len(str):
+        # if candid_items is only one item then we need to sort them based on their number
+        # of occurrence in the input that is why we separate them
+        if j < len(str):
+            candid_items = []
             f = 0
-
             for i in list_iter:
                 in_list, weight = find_prefix(root, str[j:len(str)] + i)
 
-                if weight == max:
+                if weight == max_weight:
                     print(str[j:len(str)] + i, weight)
                     f = f + 1
-                    candid.append(i)
-                if max < weight:
+                    candid_items.append(i)
+                if max_weight < weight:
                     print(str[j:len(str)] + i, weight)
                     f = 1
-                    str_max = str[j:len(str)] + i
-                    max = weight
-                    candid = []
-                    candid.append(i)
+                    max_weight = weight
+                    candid_items = []
+                    candid_items.append(i)
             print("adding and removing ", str[j])
-            myList.removeNode(str[j])
-            myList.addNode(str[j])
+            linked_list.remove_node(str[j])
+            linked_list.add_node(str[j])
         else:
             # sort items by number of occurance
-            print("candid before :", candid)
-            bubble_sort(candid)
-            print("candid after", candid) # when swaping items in the list in a function the actual list changes
+            print("candid_items before :", candid_items)
+            bubble_sort(candid_items)
+            print("candid_items after", candid_items)
+            # when swapping items in the list in a function the actual list changes
 
-        if f == 1:  # age candid 1 item dasht
+        if f == 1:  # age candid_items 1 item dasht
             j = j + 1
             while j < len(str):
-                #print("J is : ", str[j])
                 print("adding and removing ", str[j])
-                myList.removeNode(str[j])
-                myList.addNode(str[j])
+                linked_list.remove_node(str[j])
+                linked_list.add_node(str[j])
                 j = j + 1
-            print(" i is : ", candid[0])
-            print("adding and removing ", candid[0])
-            myList.removeNode(candid[0])
-            myList.addNode(candid[0])
-            candid = []
+            print(" i is : ", candid_items[0])
+            print("adding and removing ", candid_items[0])
+            linked_list.remove_node(candid_items[0])
+            linked_list.add_node(candid_items[0])
+            candid_items = []
             break
 
-        # candid = []
         j = j + 1
-    # if all itmes have the same weight then candid is 26
-    # if candid is 26 it means that all patterns had the same weights or a new character has been
+    # if all items have the same weight then candid_items is 26
+    # if candid_items is 26 it means that all patterns had the same weights or a new character has been
     # added to the front like it was : "aaababa" now it is "aaababac" so we should check if
     # the last item is new or previously seen
 
-    for candids in candid:
-        print("adding and removing", candids)
-        myList.removeNode(candids)
-        myList.addNode(candids)
+    for candid in candid_items:
+        print("adding and removing", candid)
+        linked_list.remove_node(candid)
+        linked_list.add_node(candid)
 
-    #if f != 26:
-     #   for candids in candid:
-      #      print("adding and removing",candids)
-       #     myList.removeNode(candids)
-        #    myList.addNode(candids)
-    #else:
-     #   print("no candids sort by number of occurance of each character")
-      #  print("candid is : ", candid)
 
-        #print("CANDID BEFORE :", candid)
-        #x = bubble_sort(candid)
-        #print("CANDID AFTER :",x)
-        #for candids in x:
-         #   myList.removeNode(candids)g
-          #  myList.addNode(candids)
+def contextbased_listupdate(root_node, str, context_len):
 
-def make_tri(root, str, index):
-
-    for i in range(index):
-        substr = str[i:index]
+    for i in range(context_len):
+        substr = str[i:context_len]
         print(substr)
-        _add(root, substr)
+        _add(root_node, substr)
 
-    for j in range(index, len(str)):
-        for k in range(index):
-            substr = str[j - index + 1 + k: j + 1]
-            add(root, substr)
+    for j in range(context_len, len(str)):
+        for k in range(context_len):
+            substr = str[j - context_len + 1 + k: j + 1]
+            add(root_node, substr)
 
-        update_list(myList, str[j - index + 1: j + 1], index)
-        myList.printNode()
+        update_list(myList, str[j - context_len + 1: j + 1], context_len)
+        myList.print_node()
+
 
 if __name__ == "__main__":
+
     root = TrieNode('*')
     myList = LinkedList()
     myList.make_alphabet_list()
 
-    str = "aababca"
+    str = "aababcab"
 
     index = 3
-    make_tri(root, str, index)
+    contextbased_listupdate(root, str, index)
 
-    #sorted = bubble_sort(['b', 'a'])
-    #print(sorted)
+    # print(find_weight(root,"ab"))
+    # myList.print_node()
 
 
 
